2800ea01b60df21b0049cde7506d621f2019fbda
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-08-25 00:46:38 +1000

    Add code to accept a parameter in an action method (2. The MVC Pattern - Pass parameters to controller actions).

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -13,5 +13,19 @@ namespace LearningAspDotNetCoreMVC.Controllers
             return new ContentResult { Content = "Blog Posts" };
             //return View();
         }
+
+        // Passes the string from the URL in the 'id' portion to the action method:
+        // ./blog/post/XXX
+        // Alternatively URL can be entered in the following manner:
+        // ./blog/post?id=XXX
+        // If the parameter passed in is nullable e.g. int? the result will
+        // either be a value that belongs to the datatype or else it is null.
+        // A default value can also be assigned to be used when the value given
+        // does not belong to the specified datatype.
+        public IActionResult Post(int? id = -1)
+        {
+            
+            return new ContentResult { Content = id.ToString() };
+        }
     }
 }
