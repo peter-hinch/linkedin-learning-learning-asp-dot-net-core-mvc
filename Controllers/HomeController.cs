1cb9f19b06098b81faa7633343ad1e0cbe417796
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-08-24 23:58:52 +1000

    Create Home and Blog controllers (2. The MVC Pattern - Handle requests with controllers).

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningAspDotNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Hello, ASP.NET Core MVC!" };
+            //return View();
+        }
+    }
+}
