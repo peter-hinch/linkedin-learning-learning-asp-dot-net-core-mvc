74d3af2543e2fd7e481c55852db14a8df294cd8c
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-09-16 22:59:47 +1000

    Create and insert the MonthlySpecialsViewComponent (5. Creating Resusable Components - Enhancing a view component with logic and dynamic markup).

---
+++ ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningAspDotNetCoreMVC.ViewComponents
+{
+    // To define a ViewComponent the class must satisfy at least one of the
+    // following conditions:
+    // - Name of the class must end with the suffix ViewComponent
+    // - Class must have the ViewComponent decorator
+    // - Extend from the ViewComponent base class
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        // Declare a public method named Invoke that returns a string.
+        public string Invoke()
+        {
+            return "TODO: Show monthly specials";
+        }
+    }
+}
